bdd421511d6f7703bc96ce7299d288f8955d1126
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2025-03-24 16:01:40 +0100

    Fix the code: add try/catch and new short operator

--- main.js
+++ main.js
@@ -1,29 +1,33 @@
 // EX - Dashboard della città
 // async function getDashboardData(query) {
-//     const cityPromise = fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}`).then((resp) => resp.json())
-//     // console.log(cityPromise);
+//     try {
+//         const cityPromise = fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}`).then((resp) => resp.json())
+//         // console.log(cityPromise);
 
-//     const weathersPromise = fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${query}`).then((resp) => resp.json())
-//     // console.log(weathersPromise);
+//         const weathersPromise = fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${query}`).then((resp) => resp.json())
+//         // console.log(weathersPromise);
 
-//     const airportsPromise = fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`).then((resp) => resp.json())
-//     // console.log(airportsPromise);
+//         const airportsPromise = fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`).then((resp) => resp.json())
+//         // console.log(airportsPromise);
 
-//     const [city, wheater, airport] = await Promise.all([cityPromise, weathersPromise, airportsPromise])
-//     // console.log([city, wheater, airport]);
+//         const [city, wheater, airport] = await Promise.all([cityPromise, weathersPromise, airportsPromise])
+//         // console.log([city, wheater, airport]);
 
-//     const result = {
-//         city: city.length > 0 ? city[0].name : null,
-//         country: city.length > 0 ? city[0].country : null,
-//         temperature: wheater.length > 0 ? wheater[0].temperature : null,
-//         weather: wheater.length > 0 ? wheater[0].weather_description : null,
-//         airport: airport.length > 0 ? airport[0].name : null
-//     };
+//         const result = {
+//             city: city[0]?.name ?? null,
+//             country: city[0]?.country ?? null,
+//             temperature: wheater[0]?.temperature ?? null,
+//             weather: wheater[0]?.weather_description ?? null,
+//             airport: airport[0]?.name ?? null
+//         };
 
-//     return result;
+//         return result;
+//     } catch (error) {
+//         throw new Error(`Errore nel recupero dei dati: ${error.message}`)
+//     }
 // }
 
-// getDashboardData('vienna')
+// getDashboardData('paris')
 //     .then(data => {
 //         console.log('Dasboard data:', data);
 //         let message = '';
@@ -43,49 +47,53 @@
 
 // Bonus 2 - Promise.allSettled()
 async function getDashboardData(query) {
-    const cityPromise = fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}`).then((resp) => resp.json())
-    // console.log(cityPromise);
+    try {
+        const cityPromise = fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}`).then((resp) => resp.json())
+        // console.log(cityPromise);
 
-    const weathersPromise = fetch(`https://www.meteofittizio.it`).then((resp) => resp.json())
-    // console.log(weathersPromise);
+        const weathersPromise = fetch(`https://www.meteofittizio.it`).then((resp) => resp.json())
+        // console.log(weathersPromise);
 
-    const airportsPromise = fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`).then((resp) => resp.json())
-    // console.log(airportsPromise);
+        const airportsPromise = fetch(`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`).then((resp) => resp.json())
+        // console.log(airportsPromise);
 
-    const [cityResult, weathersResult, airportsResult] = await Promise.allSettled([cityPromise, weathersPromise, airportsPromise])
-    // console.log([cityResult, weathersResult, airportsResult]);
+        const [cityResult, weathersResult, airportsResult] = await Promise.allSettled([cityPromise, weathersPromise, airportsPromise])
+        // console.log([cityResult, weathersResult, airportsResult]);
 
-    const data = {};
+        const data = {};
 
-    if (cityResult.status === "fulfilled") {
-        const city = cityResult.value[0];
-        data.city = city ? city.name : null;
-        data.country = city ? city.country : null
-    } else {
-        console.error(`Errore nel city fetch: ${cityResult.reason}`)
-        data.city = null;
-        data.country = null;
+        if (cityResult.status === "fulfilled") {
+            const city = cityResult.value[0];
+            data.city = city ? city.name : null;
+            data.country = city ? city.country : null
+        } else {
+            console.error(`Errore nel city fetch: ${cityResult.reason}`)
+            data.city = null;
+            data.country = null;
+        }
+
+        if (weathersResult.status === "fulfilled") {
+            const weather = weathersResult.value[0]
+            data.temperature = weather ? weather.temperature : null;
+            data.weather = weather ? weather.weather_description : null;
+        } else {
+            console.error(`Errore nel weather fetch: ${weathersResult.reason}`)
+            data.temperature = null;
+            data.weather = null;
+        }
+
+        if (airportsResult.status === "fulfilled") {
+            const airport = airportsResult.value[0]
+            data.airport = airport ? airport.name : null;
+        } else {
+            console.error(`Errore nel airport fetch: ${airportsResult.reason}`)
+            data.airport = null;
+        }
+
+        return data;
+    } catch (error) {
+        throw new Error(`Errore nel recupero dei dati: ${error.message}`)
     }
-
-    if (weathersResult.status === "fulfilled") {
-        const weather = weathersResult.value[0]
-        data.temperature = weather ? weather.temperature : null;
-        data.weather = weather ? weather.weather_description : null;
-    } else {
-        console.error(`Errore nel weather fetch: ${weathersResult.reason}`)
-        data.temperature = null;
-        data.weather = null;
-    }
-
-    if (airportsResult.status === "fulfilled") {
-        const airport = airportsResult.value[0]
-        data.airport = airport ? airport.name : null;
-    } else {
-        console.error(`Errore nel airport fetch: ${airportsResult.reason}`)
-        data.airport = null;
-    }
-
-    return data;
 }
 
 getDashboardData('london')
